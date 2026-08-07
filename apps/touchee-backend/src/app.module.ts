b9ff6426717f9928539aee5637f9cd0d80da8693
unknown <hoang.viet.c4f@gmail.com>
2026-08-07 12:31:51 +0700

    chore: initize core module dependencies
    Set up global ConfigModule for environment variables
    >  - Import DatabaseModule and AuthModule

--- apps/touchee-backend/src/app.module.ts
+++ apps/touchee-backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from '@app/database';
+import { AuthModule } from '@app/auth';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// import { UsersModule } from './users/users.module';
+// import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    AuthModule,
+    // UsersModule,
+    // PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
