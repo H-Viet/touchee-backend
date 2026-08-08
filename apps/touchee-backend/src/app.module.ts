bf408791bdecf0079807def50e896a60272578f0
unknown <hoang.viet.c4f@gmail.com>
2026-08-08 19:21:32 +0700

    feat: update app.module with comment module

--- apps/touchee-backend/src/app.module.ts
+++ apps/touchee-backend/src/app.module.ts
@@ -7,6 +7,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { CommunitiesModule } from './communities/communities.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { CommunitiesModule } from './communities/communities.module';
     UsersModule,
     PostsModule,
     CommunitiesModule,
+    CommentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
